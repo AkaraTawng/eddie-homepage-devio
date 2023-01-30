407e07c74cf4aaf21c633fc150e5b2617efa63f6
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-01-31 06:36:07 +0700

    add onclick hamburger icon switch functionality

---
+++ app.js
@@ -0,0 +1,16 @@
+const menuBox = document.querySelector(".menu-box");
+const menuIcons = document.querySelectorAll(".menu-icon");
+const menuOpen = document.querySelector("#menu-open");
+const menuClose = document.querySelector("#menu-close");
+
+
+
+menuIcons.forEach(menuIcon => {
+    menuIcon.onclick = function(){
+        menuBox.classList.toggle('open-menu');
+        menuOpen.classList.toggle('open')
+        menuClose.classList.toggle('open')
+    }
+})
+
+

d182bac1fea8d48d86a6f9c527c261ce8bd437aa
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-01-31 06:48:00 +0700

    change menu items text-shadow color to blue

--- app.js
+++ app.js
@@ -3,14 +3,12 @@ const menuIcons = document.querySelectorAll(".menu-icon");
 const menuOpen = document.querySelector("#menu-open");
 const menuClose = document.querySelector("#menu-close");
 
-
-
 menuIcons.forEach(menuIcon => {
     menuIcon.onclick = function(){
         menuBox.classList.toggle('open-menu');
         menuOpen.classList.toggle('open')
         menuClose.classList.toggle('open')
     }
-})
+});
 
 

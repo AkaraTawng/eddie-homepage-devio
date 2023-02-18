4b786cc143d3ad5a3f093edb13e2717d329e4fa4
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-02-19 05:41:23 +0700

    add new observer. add callback and options

--- app.js
+++ app.js
@@ -6,24 +6,12 @@ const menuOpen = document.querySelector("#menu-open");
 const menuClose = document.querySelector("#menu-close");
 const serviceCardsOuterContainer = document.querySelector(".service-cards-outer-container");
 
-const serviceCards = document.querySelectorAll(".service-card-outer-container");
+let options = {};
 
-menuIcons.forEach(menuIcon => {
-    menuIcon.onclick = function(){
-        menuBox.classList.toggle('open-menu');
-        menuOpen.classList.toggle('open')
-        menuClose.classList.toggle('open')
-    }
-});
-
-function serviceCardCallback(entries){
-    entries.forEach(entry => {
-        console.log(entry)
+let callback = (entries, observer) => {
+    entries.forEach((entry) => {
+        console.log(entry);
     })
-};
+}
 
-const serviceCardOptions = {};
-
-const serviceCardsObs = new IntersectionObserver(serviceCardCallback, serviceCardOptions);
-
-serviceCardsObs.observe(serviceCardsOuterContainer)
+let observer = new IntersectionObserver(callback, options);

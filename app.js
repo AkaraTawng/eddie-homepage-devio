53f395e6e47e893694a4ebc025d13deb6cde47a7
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-02-19 06:51:39 +0700

    add fadeInUp scroll effect to service cards

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
-import 'animate.css';
+
 
 const menuBox = document.querySelector(".menu-box");
 const menuIcons = document.querySelectorAll(".menu-icon");
 const menuOpen = document.querySelector("#menu-open");
 const menuClose = document.querySelector("#menu-close");
 const serviceCardsOuterContainer = document.querySelector(".service-cards-outer-container");
+let target = document.querySelectorAll('.service-card-outer-container');
 
-let options = {};
 
-let callback = (entries, observer) => {
-    entries.forEach((entry) => {
-        console.log(entry);
+let observer = new IntersectionObserver((entries) => {
+
+    entries.forEach(entry => {
+        entry.target.classList.toggle('animate__fadeInUp', entry.isIntersecting)
+        if(entry.isIntersecting) observer.unobserve(entry.target)
     })
-}
+  console.log(entries);
+}, 
+{
+    threshold: 0,
+    // rootMargin: "-100px",
+}); 
 
-let observer = new IntersectionObserver(callback, options);
+target.forEach(target => {
+    observer.observe(target);
+})

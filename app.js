3f770186331daf3b2ed904b20c8a00788033d6e6
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-02-21 06:36:44 +0700

    add intersection observer scroll animation to hero email contact container and portfolio image container even

--- app.js
+++ app.js
@@ -7,28 +7,57 @@ const menuClose = document.querySelector("#menu-close");
 const serviceCardsOuterContainer = document.querySelector(".service-cards-outer-container");
 let serviceCards = document.querySelectorAll('.service-card-outer-container');
 const largeScreenQuery = window.matchMedia("(min-width: 1280px)");
+const heroEmailContactContainer = document.querySelector('#hero-email-contact-container');
+const portfolioItemsEven = document.querySelectorAll(".portfolio-item-container.even");
+const portfolioItemsOdd = document.querySelectorAll(".portfolio-item-container.odd");
 
+/* IO is defaulting to 'isIntersecting: true' and intersectionRatio: 1'. Find way to default to 'isIntersecting: false' and intersectionRatio: 0' */
+const emailObserverOptions = { threshold: 0.1, rootMargin: '0px'};
 if(largeScreenQuery.matches){
-    //header join email left slide in
-    
+    //hero join email left slide in
+    let emailObserver = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            // if(!entry.isIntersecting) return;
+            // console.log(entry)
+            entry.target.classList.toggle('animate__slideInLeft')
+            // emailObserver.unobserve(entry.target)
+        })
+    }, emailObserverOptions)
+
+    emailObserver.observe(heroEmailContactContainer);
 
     //service cards staggered slide up
-    let observer = new IntersectionObserver((entries) => {
+    let serviceCardsObserver = new IntersectionObserver((entries) => {
     entries.forEach((entry, index) => {
         setTimeout(() => {
             entry.target.classList.toggle('animate__fadeInUpBig', entry.isIntersecting)
-            if(entry.isIntersecting) observer.unobserve(entry.target)
-        }, 200 * (index + 1));
+            if(entry.isIntersecting) serviceCardsObserver.unobserve(entry.target)
+        }, 300 * (index + 1));
       
     })
 }, 
 {
     root: null,
     threshold: 0,
-    rootMargin: "50px",
+    rootMargin: "150px",
 }); 
 
 serviceCards.forEach(serviceCard => {
-    observer.observe(serviceCard);
+    serviceCardsObserver.observe(serviceCard);
 })
 }
+
+
+const portfolioItemsEvenObserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting) return;
+        // console.log(entry)
+        entry.target.classList.add('animate__slideInRight');
+        portfolioItemsEvenObserver.unobserve(entry.target);
+    })
+}, );
+
+portfolioItemsEven.forEach(portfolioItem => {
+    portfolioItemsEvenObserver.observe(portfolioItem);
+});
+
